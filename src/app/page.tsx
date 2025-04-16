ea7fef855aae7f9eb1dc31b940341b4f1d7e4163
Rasmus0000 <rasmuskarm3@gmail.com>
2025-04-16 17:03:45 +0300

    removed unused variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 import PalgakalkulaatorLayout from "@/components/PalgakalkulaatorLayout";
-import Palgakalkulaator from "@/components/Palgakalkulaator";
 import PalgakalkulaatorContextWrapper from "@/components/PalgakalkulaatorContextWrapper";
 
 export default function Home() {
   return (
     <section className="flex min-h-screen flex-col items-start justify-start p-5 lg:p-16">
-           
-        <h2 className="font-title text-[2rem] font-normal">Palgakalkulaator</h2>
+      <h2 className="font-title text-[2rem] font-normal">Palgakalkulaator</h2>
 
-        <PalgakalkulaatorLayout>
-          <PalgakalkulaatorContextWrapper/>
-        </PalgakalkulaatorLayout>
+      <PalgakalkulaatorLayout>
+        <PalgakalkulaatorContextWrapper />
+      </PalgakalkulaatorLayout>
     </section>
   );
 }
